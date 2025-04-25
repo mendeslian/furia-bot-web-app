7e5588375c5e86f4581bb71bf29f7b2af2c543d2
lian mendes <nailptm@gmail.com>
2025-04-25 19:05:29 -0300

    feat: creating HeroSection

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,55 +2,9 @@
 import Chat from "../../components/Chat";
 import Header from "../../components/Header";
 import TeamSection from "../../components/TeamSection";
-
-// Assets
-import FuriaBackground from "../../assets/furia-background.png";
+import HeroSection from "../../components/HeroSection";
 
 export default function Home() {
-  function HeroSection() {
-    return (
-      <section
-        className="w-full h-180 relative flex flex-col justify-center items-center"
-        aria-labelledby="welcome-heading"
-      >
-        <div
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-50"
-          style={{
-            backgroundImage: `url(${FuriaBackground})`,
-          }}
-          aria-hidden="true"
-        >
-          <div className="absolute inset-0 bg-black/50" />
-        </div>
-
-        <div className="max-w-5xl w-full mx-auto relative z-10 flex flex-col items-start gap-6 px-5">
-          <div className="text-left max-w-130">
-            <h1
-              id="welcome-heading"
-              className="text-white text-4xl font-bold mb-4"
-            >
-              Seja bem vindo!
-            </h1>
-            <h2 className="text-white/90 text-lg font-medium">
-              Envie suas perguntas, tire dúvidas, converse com a gente e
-              acompanhe todas as novidades do cenário de CS2 e, claro, do melhor
-              time do Brasil! <i>#GoFURIA</i>
-            </h2>
-          </div>
-          <div className="flex gap-4">
-            <a
-              href="#chat"
-              className="h-10 flex items-center justify-center gap-2 px-4 rounded-sm cursor-pointer duration-200 drop-shadow-md text-md font-semibold disabled:cursor-default bg-neutral-50 hover:bg-neutral-200 disabled:bg-neutral-300 text-neutral-900"
-              aria-label="Começar a conversar com o Furia BOT"
-            >
-              Começar agora
-            </a>
-          </div>
-        </div>
-      </section>
-    );
-  }
-
   return (
     <>
       <Header />
