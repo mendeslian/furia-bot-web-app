d1b24062b3c35e40d2aa49babb4db698a60e79fa
lian mendes <nailptm@gmail.com>
2025-04-26 14:13:28 -0300

    fix: adjusting BrandSection animations

--- src/components/BrandSection/index.jsx
+++ src/components/BrandSection/index.jsx
@@ -7,7 +7,6 @@ import Lenovo from "../../assets/brands/lenovo.svg";
 import PokerStars from "../../assets/brands/pokerstars.svg";
 
 export default function BrandSection() {
-  const brandsClass = "w-40 h-16 absolute left-[100%] scroll-animation";
   const brands = [
     Adidas,
     CruzeiroDoSul,
@@ -20,7 +19,7 @@ export default function BrandSection() {
   return (
     <section className="w-full h-80 bg-white flex items-center border-y-1 border-neutral-700">
       <div
-        className="w-full max-w-5xl h-20 mx-auto flex items-center px-5 relative overflow-hidden"
+        className="w-full max-w-5xl h-20 mx-auto relative overflow-hidden"
         style={{
           maskImage:
             "linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,1) 20%, rgba(0,0,0,1) 80%, rgba(0,0,0,0))",
@@ -29,16 +28,17 @@ export default function BrandSection() {
         {brands.map((brand, index) => (
           <div
             key={index}
-            className={brandsClass}
+            className="w-40 h-16 scroll-animation"
             style={{
-              animationDelay: `calc(20s / 6 * (6 - ${index + 1}))`,
+              left: "max(calc(40rem), 100%)",
+              animationDelay: `calc(20s / 6 * (6 - ${index + 1}) * -1)`,
             }}
           >
             <img
               src={brand}
               alt="Brand"
               draggable={false}
-              className="w-full h-full select-none"
+              className="w-full h-full select-none object-contain"
             />
           </div>
         ))}
