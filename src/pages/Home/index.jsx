b3ca4fe0e18453fff83ec6d873f245b1a3d2f99e
lian mendes <nailptm@gmail.com>
2025-04-26 01:07:01 -0300

    feat: adding footer to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import Chat from "../../components/Chat";
 import Header from "../../components/Header";
 import TeamSection from "../../components/TeamSection";
 import HeroSection from "../../components/HeroSection";
+import Footer from "../../components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <TeamSection />
       <Chat />
+      <Footer />
     </>
   );
 }
