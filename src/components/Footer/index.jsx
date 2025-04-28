1dac98140d87cd2b2891fb6a3ea3734f28523bd8
lian mendes <nailptm@gmail.com>
2025-04-27 23:53:10 -0300

    style: adjusting footer date

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -94,7 +94,7 @@ export default function Footer() {
         <hr className="border-neutral-900" />
         <div className="flex flex-col md:flex-row justify-between items-center">
           <p className="text-neutral-400 text-xs">
-            © 2024 Furia. All Rights Reserved.
+            © 2025 Furia. All Rights Reserved.
           </p>
           <div className="flex gap-4 mt-4 md:mt-0">
             <a
