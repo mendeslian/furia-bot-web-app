41d61ff278245fb306f16be03025cdbbf590082a
lian mendes <nailptm@gmail.com>
2025-04-27 21:17:11 -0300

    style: adding blur animation to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+
+// Assets
 import FuriaLogo from "../../assets/furia-esports-logo.svg";
 import FuriaTextLogo from "../../assets/furia-text-logo.svg";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 20;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolled]);
+
   const headerOptions = [
     {
       id: "store",
@@ -24,7 +43,11 @@ export default function Header() {
     "text-white text-xs uppercase font-light tracking-[1px] cursor-pointer duration-200 relative px-1 hover:text-yellow-500";
 
   return (
-    <header className="fixed top-0 left-0 w-full h-20 bg-neutral-900/50 backdrop-blur-sm z-50">
+    <header
+      className={`fixed top-0 left-0 w-full h-20 z-50 transition-all duration-300 ${
+        scrolled ? "bg-neutral-900/50 backdrop-blur-sm" : "bg-transparent"
+      }`}
+    >
       <div className="max-w-5xl w-full mx-auto flex items-center justify-between px-4 h-full">
         <div className="flex gap-2">
           <img
